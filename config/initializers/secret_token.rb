0e764203ed0e6eadd2c81a717cfaf9154ee7c02a
bhzunami <nmauchle@gmail.com>
2013-03-18 18:36:54 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Castage::Application.config.secret_token = 'f57382fdc4563b607b881e47d3f3d79c3f941382c52d467a5549dd578f6d2202e628347e7dc63c39d408419acd4a1de425b554e04100b5456d953293b9333f3e'
