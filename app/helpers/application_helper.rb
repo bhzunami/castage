f4c63b42ca6b9569d75d504e9380592e25bd2aa1
bhzunami <nmauchle@gmail.com>
2013-03-20 11:14:02 +0100

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	# Returns the full title on a per-page basis.
+	def full_title( page_title )
+		base_title = "Ferienhaus vor Weissenfluh"
+
+		return "#{base_title} | #{page_title}" unless page_title.empty?
+		return base_title
+	end
 end
