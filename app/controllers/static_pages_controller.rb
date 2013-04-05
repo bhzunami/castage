24e6ebb5670d4717fd76eda67b0a807eb272a179
bhzunami <nmauchle@gmail.com>
2013-04-05 18:20:07 +0200

    Added pictures

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,9 +4,9 @@ class StaticPagesController < ApplicationController
 
   def home
   	@images = []
-  	@images << { url: 'img_1.jpg', alt: 'IMAGE1', title: 'Willkommen', 
+  	@images << { url: 'Haus1.jpg', alt: 'IMAGE1', title: 'Willkommen', 
   				description: "Willkommen auf der Weissenfluh. Geniessen sie Ihre Ferien in Ruhe in einem gemütlichen Chalet mitten im Haslitall mit einer grandiosen Aussicht auf den See."} 
-  	@images << { url: 'img_2.jpg', alt: 'IMAGE2', title: 'Natur erleben',
+  	@images << { url: 'Haus2.jpg', alt: 'IMAGE2', title: 'Natur erleben',
   				description: 'Geniessen sie die Natur und die freie Landluft.'} 
   	@images << { url: 'img_3.jpg', alt: 'IMAGE3', title: 'Title Bild 3'} 
 
@@ -16,9 +16,9 @@ class StaticPagesController < ApplicationController
 
   def property_description
   	@images = []
-  	@images << { url: 'img_1.jpg', alt: 'IMAGE1', title: 'Haus', 
+  	@images << { url: 'Haus1.jpg', alt: 'IMAGE1', title: 'Haus', 
   				description: ' Gemuütliches und gut eingerichtetes 4-Zimmer-Chalet an ruhiger Lage mit wunderbarer Aussicht ins Haslital.'} 
-  	@images << { url: 'img_2.jpg', alt: 'IMAGE2', title: 'Als Famile',
+  	@images << { url: 'Haus2.jpg', alt: 'IMAGE2', title: 'Als Famile',
   				description: 'Geniessen Sie die Zeit als Familie. Mit bis zu 6 Schlafmöglichkeiten ist das Haus auch fuer eine Grossfamilie bestens geeignet.'} 
   	@images << { url: 'img_3.jpg', alt: 'IMAGE3', title: 'Romantische Abende',
   				description: 'Entspannen Sie sich vor einem knisternden, warmen Chminee, in einer kuscheligen Stube.'} 
@@ -26,8 +26,8 @@ class StaticPagesController < ApplicationController
 
   def reservation
   	@images = []
-  	@images << { url: 'img_1.jpg', alt: 'IMAGE1', title: 'Reservation', description: 'More Bla bli blu for the description of the picutre. Should be a little bit more bla bli blu'} 
-  	@images << { url: 'img_2.jpg', alt: 'IMAGE2', title: 'Title Bild 2'} 
+  	@images << { url: 'Haus1.jpg', alt: 'IMAGE1', title: 'Reservation', description: 'More Bla bli blu for the description of the picutre. Should be a little bit more bla bli blu'} 
+  	@images << { url: 'Haus2.jpg', alt: 'IMAGE2', title: 'Title Bild 2'} 
   	@images << { url: 'img_3.jpg', alt: 'IMAGE3', title: 'Title Bild 3'} 
   end
   
