b60a40810e51e382e79256c9ad3d49e45fd0d2b6
Nicolas Mauchle <nmauchle@gmail.com>
2013-08-15 17:08:34 +0200

    Add Mailserver

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Castage::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'app15258626@heroku.com',
+  :password => 'rukgjw9w',
+  :domain => 'castage.heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
